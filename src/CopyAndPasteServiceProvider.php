3c2a977308b8b9d0d9b7adf4d7e57044f7649ae1
Arnel Basiliote <arnel.basiliote@copypaste.ph>
2018-11-08 17:24:38 +0800

    initial copyandpaste docker development

---
+++ src/CopyAndPasteServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vessel;
+
+use Illuminate\Support\ServiceProvider;
+
+class VesselServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+        $this->publishes([
+            DEV_PATH.'/docker-files/docker' => base_path('docker'),
+            DEV_PATH.'/docker-files/docker-compose.yml' => base_path('docker-compose.yml'),
+            DEV_PATH.'/docker-files/vessel' => base_path('develop'),
+        ]);
+    }
+
+    public function register()
+    {
+        if (! defined('DEV_PATH')) {
+            define('DEV_PATH', realpath(__DIR__.'/../'));
+        }
+    }
+
+}
